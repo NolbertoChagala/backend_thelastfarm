e40e0423008d5749fdc273d6071f8b905c057531
NolbertoChagala <21393137@utcancun.edu.mx>
2025-11-24 00:22:07 -0500

    backrender

--- src/firebase.js
+++ src/firebase.js
@@ -1,19 +1,7 @@
 // firebase.js
 import admin from "firebase-admin";
-import path from "path";
-import { fileURLToPath } from "url";
-import fs from "fs/promises";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-const serviceAccountPath = path.join(__dirname, "config", "serviceAccountKey.json");
-
-console.log("Buscando archivo en:", serviceAccountPath);
-
-const serviceAccount = JSON.parse(
-  await fs.readFile(serviceAccountPath, "utf8")
-);
+const serviceAccount = JSON.parse(process.env.SERVICE_ACCOUNT_KEY);
 
 if (!admin.apps.length) {
   admin.initializeApp({
