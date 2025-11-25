d14667764113054b93ae86fc9907bf02a14d3267
NolbertoChagala <21393137@utcancun.edu.mx>
2025-11-25 18:49:29 -0500

    localfuncionando

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,42 @@
 // firebase.js
 import admin from "firebase-admin";
+import path from "path";
+import { fileURLToPath } from "url";
+import fs from "fs";
 
-const serviceAccount = JSON.parse(process.env.SERVICE_ACCOUNT_KEY);
+
+const isLocal = process.env.NODE_ENV !== "production";
+
+let serviceAccount;
+
+if (isLocal) {
+
+  // ===============================
+  // ✔ MODO LOCAL (usa el archivo)
+  // ===============================
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+
+  const localPath = path.join(__dirname, "config", "serviceAccountKey.json");
+  console.log("🔍 Cargando serviceAccount local:", localPath);
+
+  try {
+    const raw = fs.readFileSync(localPath, "utf8");
+    serviceAccount = JSON.parse(raw);
+    console.log("✅ serviceAccountKey.json cargado (LOCAL)");
+  } catch (err) {
+    console.error("❌ ERROR: No se pudo cargar serviceAccountKey.json");
+    throw err;
+  }
+} else {
+
+  // ============================================
+  // ✔ MODO PRODUCCIÓN (Render → variable env)
+  // ============================================
+  
+  console.log("☁️ Usando SERVICE_ACCOUNT_KEY desde variables de entorno");
+  serviceAccount = JSON.parse(process.env.SERVICE_ACCOUNT_KEY);
+}
 
 if (!admin.apps.length) {
   admin.initializeApp({
